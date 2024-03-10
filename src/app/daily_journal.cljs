9269d05989c0c77d616a84dc740395489f07f186
Frederic Merizen <frederic@merizen.org>
2024-03-10 19:05:23 +0100

    variable font sizes

--- src/app/daily_journal.cljs
+++ src/app/daily_journal.cljs
@@ -1,26 +1,31 @@
 (ns app.daily-journal
   (:require
-    [akiroz.re-frame.storage :as storage]
-    [cljss.core :as css]
-    [cljss.reagent]
-    [goog.string :as gstring]
-    [goog.string.format]
-    [medley.core :refer [assoc-some]]
-    [re-frame.core :as rf]
-    [taoensso.tempura :as tempura]
-    [reagent.core :as r])
+   [akiroz.re-frame.storage :as storage]
+   [cljss.core :as css]
+   [cljss.reagent]
+   [goog.string :as gstring]
+   [goog.string.format]
+   [medley.core :refer [assoc-some]]
+   [re-frame.core :as rf]
+   [taoensso.tempura :as tempura]
+   [reagent.core :as r])
   (:require-macros
-    [cljss.core :refer [defstyles]]
-    [cljss.reagent :refer [defstyled]]))
+   [cljss.core :refer [defstyles]]
+   [cljss.reagent :refer [defstyled]]))
 
 (def translations
   {:fr {:page/cols "colonnes par page"
         :page/rows "lignes par page"
         :hour/cols "colonne par heure"
         :hour/rows "lignes par heure"
+        :hour/font-size "taille police heures"
+        :minute/font-size "taille police minutes"
         :day/begin "début de journée"
         :day/end "fin de journée"
         :print "imprimer"}})
+
+(def persistent-db-keys
+  [:page/rows :page/cols :hour/rows :hour/cols :day/begin :day/end :font/minutes :font/hours])
 
 (def tr
   (partial tempura/tr {:dict translations} [:fr]))
@@ -28,10 +33,10 @@
 (defn reg-event-db
   [event-id handler]
   (rf/reg-event-fx
-    event-id
-    [(storage/persist-db-keys :timetracking [:page/rows :page/cols :hour/rows :hour/cols :day/begin :day/end])]
-    (fn [{:keys [db]} event-vec]
-      {:db (handler db event-vec)})))
+   event-id
+   [(storage/persist-db-keys :timetracking persistent-db-keys)]
+   (fn [{:keys [db]} event-vec]
+     {:db (handler db event-vec)})))
 
 ;;; Page
 (def page-defaults
@@ -89,8 +94,6 @@
 (def day-defaults
   #:day{:begin 6 :end 24})
 
-(defn ensure-end-after-start [db])
-
 (reg-event-db :day/set-boundaries
               (fn [db [_ {:keys [begin end]}]]
                 (assoc-some db :day/begin begin :day/end end)))
@@ -113,11 +116,27 @@
             (fn [[begin end] _]
               {:begin begin :end end}))
 
+;;; Font
+(def font-defaults
+  #:font{:minutes {:size 6}
+         :hours   {:size 96}})
+
+(defn font-size-path [kind]
+  [(keyword "font" (name kind)) :size])
+
+(reg-event-db :font/set-size
+              (fn [db [_ kind size]]
+                (assoc-in db (font-size-path kind) size)))
+
+(rf/reg-sub :font/size
+            (fn [db [_ kind]]
+              (get-in db (font-size-path kind))))
+
 ;;; Init
 (reg-event-db
-  :initialize
-  (fn [db _]
-    (merge page-defaults hour-defaults day-defaults db)))
+ :initialize
+ (fn [db _]
+   (merge page-defaults hour-defaults day-defaults font-defaults db)))
 
 ;;; Css
 (defn css-repeat [attr]
@@ -140,12 +159,12 @@
 
 (defstyled hours :div
   {:grid-area "1/1"
-   :font-size "96pt"
+   :font-size :font-size
    :color "#ddd"
    :font-weight "bold"})
 
 (defstyled minutes :div
-  {:font-size "6pt"
+  {:font-size :font-size
    :padding "1mm"
    :display "grid"
    :grid-area "1/1"
@@ -154,27 +173,28 @@
 
 (defn hour [h]
   [hour-block
-   [hours h]
-   [minutes @(rf/subscribe [:hour/dimensions])
+   [hours {:font-size (str @(rf/subscribe [:font/size :hours]) "pt")} h]
+   [minutes (assoc @(rf/subscribe [:hour/dimensions])
+                   :font-size (str @(rf/subscribe [:font/size :minutes]) "pt"))
     (let [minute-count @(rf/subscribe [:hour/minute-count])]
       (doall
-        (for [i (range 0 minute-count)
-              :let [m (-> i (* 60) (quot minute-count))]]
-          ^{:key i}
-          [:div (gstring/format "%02d" m)])))]])
+       (for [i (range 0 minute-count)
+             :let [m (-> i (* 60) (quot minute-count))]]
+         ^{:key i}
+         [:div (gstring/format "%02d" m)])))]])
 
 (defn journal []
   (let [{:keys [begin end]} @(rf/subscribe [:day/boundaries])
         hours-per-page @(rf/subscribe [:page/hours])]
     [:<>
      (doall
-       (for [h-from (range begin end hours-per-page)
-             :let [h-to (min end (+ h-from hours-per-page))]]
-         ^{:key h-from}
-         [page @(rf/subscribe [:page/dimensions])
-          (for [h (range h-from h-to)]
-            ^{:key h}
-            [hour (mod h 24)])]))]))
+      (for [h-from (range begin end hours-per-page)
+            :let [h-to (min end (+ h-from hours-per-page))]]
+        ^{:key h-from}
+        [page @(rf/subscribe [:page/dimensions])
+         (for [h (range h-from h-to)]
+           ^{:key h}
+           [hour (mod h 24)])]))]))
 
 (defstyled label :label
   {:display "block"})
@@ -208,6 +228,8 @@
    [int-control (tr [:hour/rows]) @(rf/subscribe [:hour/rows]) #(rf/dispatch [:hour/set-dimensions {:rows %}])]
    [int-control (tr [:day/begin]) @(rf/subscribe [:day/begin]) #(rf/dispatch [:day/set-boundaries {:begin %}])]
    [int-control (tr [:day/end]) @(rf/subscribe [:day/end]) #(rf/dispatch [:day/set-boundaries {:end %}])]
+   [int-control (tr [:hour/font-size]) @(rf/subscribe [:font/size :hours]) #(rf/dispatch [:font/set-size :hours %])]
+   [int-control (tr [:minute/font-size]) @(rf/subscribe [:font/size :minutes]) #(rf/dispatch [:font/set-size :minutes %])]
    [:input {:type "button"
             :value (tr [:print])
             :on-click #(js/print)}]])
@@ -216,3 +238,4 @@
   [:<>
    [controls]
    [journal]])
+
